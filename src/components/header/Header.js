a6c54b3c113d600dfeb89d6c0d55b367d11e1443
Joker <leehawk@web.de>
2024-12-04 12:01:50 +0100

    .

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -85,11 +85,6 @@ function Header() {
               <a href="#talks">Talks</a>
             </li>
           )}
-          {viewResume && (
-            <li>
-              <a href="#resume">Resume</a>
-            </li>
-          )}
           <li>
             <a href="#contact">Contact Me</a>
           </li>
